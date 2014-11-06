19be73b0abb49f8e08dace65c960bfb10a908388
Heather Bryant <heather@rebelcompass.com>
2014-11-06 15:12:11 -0800

    Time issue resolved. Template is now showing day of week, time and am or pm. If statements for quake depth also now working.

--- note.py
+++ note.py
@@ -12,4 +12,4 @@ def note():
 
 
 if (__name__)=="__main__":
-	app.run()
+	app.run(debug = True)
--- quake.py
+++ quake.py
@@ -15,12 +15,11 @@ def get_quake():
 	quake['coordinates'] = r.json()['features'][0]['geometry']['coordinates']
 	quake['latitute'] = r.json()['features'][0]['geometry']['coordinates'][0]
 	quake['longitude'] = r.json()['features'][0]['geometry']['coordinates'][1]
-	quake['time'] = time.strftime('%m-%d-%Y %H:%M:%S', time.gmtime((r.json()['features'][0]['properties']['time'])/1000))
+	quake['time'] = datetime.fromtimestamp(r.json()['features'][0]['properties']['time']/1000)
 	quake['url'] = r.json()['features'][0]['properties']['url']
 	return quake
 
 #print get_quake()
-
 # Original pull in format for testing:
 
 # r = requests.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")
@@ -33,7 +32,7 @@ def get_quake():
 # coordinates = r.json()['features'][0]['geometry']['coordinates']
 # latitute = r.json()['features'][0]['geometry']['coordinates'][0]
 # longitude = r.json()['features'][0]['geometry']['coordinates'][1]
-# time = time.strftime('%m-%d-%Y %H:%M:%S', time.gmtime((r.json()['features'][0]['properties']['time'])/1000))
+# time = r.json()['features'][0]['properties']['time']
 # url = r.json()['features'][0]['properties']['url']
 
 # print "Event ID:", event_id
@@ -44,8 +43,5 @@ def get_quake():
 # print "Place:", place
 # print "Latitude:", latitute
 # print "Longitude:", longitude
-print "Time:", time
+# print "Time:", time
 # print "URL:", url
-
-
-#Got datetime solution here: http://stackoverflow.com/questions/12458595/convert-epoch-timestamp-in-python
