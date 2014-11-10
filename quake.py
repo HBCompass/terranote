ce779bbcc1cdc8451d18c17108f7e8fce39f6fdd
Heather Bryant <heather@rebelcompass.com>
2014-11-10 15:03:59 -0800

    Modified template to offer different summaries is quake is generated by quarry blast.

--- quake.py
+++ quake.py
@@ -17,7 +17,9 @@ def get_quake():
 	quake['longitude'] = r.json()['features'][0]['geometry']['coordinates'][1]
 	quake['time'] = datetime.fromtimestamp(r.json()['features'][0]['properties']['time']/1000)
 	quake['url'] = r.json()['features'][0]['properties']['url']
-	# get the datetime for when the data is retrieved, summary written
+	quake['type'] = r.json()['features'][0]['properties']['type']
+	quake['status'] = r.json()['features'][0]['properties']['status']
+	# get the datetime for when the summary is written
 	quake['recordtime'] = datetime.now()
 	return quake
 
