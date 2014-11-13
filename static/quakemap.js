1a04f1f092ca8f49deed3b0451e0ea073a9fdb6b
Heather Bryant <heather@rebelcompass.com>
2014-11-13 15:27:44 -0800

    Pulled in new data. Reformatted quake.html with commenting and bootstrap friendly div classes.

---
+++ static/quakemap.js
@@ -0,0 +1,22 @@
+
+
+// function initialize() {
+//   var mapOptions = {
+//     zoom: 8,
+//     center: new google.maps.LatLng(-34.397, 150.644)
+//   };
+
+//   var map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
+// }
+
+// function loadScript() {
+//   var script = document.createElement('script');
+//   script.type = 'text/javascript';
+//   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+//       'callback=initialize';
+//   document.body.appendChild(script);
+// }
+
+// window.onload = loadScript;
+
