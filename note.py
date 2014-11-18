d8708bd498f9928a9fca4dcbc145ad59661e6e69
Heather Bryant <heather@rebelcompass.com>
2014-11-17 17:17:08 -0800

    Revised html divs for better use of bootstrap. Made some style adjustments.

--- note.py
+++ note.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def note():
 	# get most recent quake record
-	quake = db_session.query(Quake).first()
+	quake = db_session.query(Quake).order_by(Quake.quake_datetime.desc()).first()
 	# pass dictionary k,v to jinja template
 	return render_template("quake.html", quake=quake)
 
 @app.route("/quakes")
 def quake_notes():
-	quakes = Quake.query.all()
+	quakes = Quake.query.order_by(Quake.quake_datetime.desc()).all()
 	return render_template("quakes.html", quakes=quakes)
 
 @app.route("/quakes/<quake_id>")
