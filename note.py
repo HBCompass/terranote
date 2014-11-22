9ae64eed6946121c51e2e59cb2ddba156f48b603
Heather Bryant <heather@rebelcompass.com>
2014-11-21 18:00:55 -0800

    Added 10 most recent quakes to home, styled quakes, added javascript to determine user location.

--- note.py
+++ note.py
@@ -3,19 +3,29 @@ from flask import session as user_session
 from model import Quake, session as db_session
 from quake import get_quake
 
+
 app = Flask(__name__)
 
 @app.route("/")
-def note():
-	# get most recent quake record
-	quake = db_session.query(Quake).order_by(Quake.quake_datetime.desc()).first()
-	# pass dictionary k,v to jinja template
-	return render_template("quake.html", quake=quake)
+def home_map():
+	# get user location from session data
+	# user_location = request.args.get("userpoint")
+	# pass user location to home.html as userlat, userlng
+	recent_quakes = db_session.query(Quake).order_by(Quake.quake_datetime.desc()).limit(10)
+	return render_template("home.html", recent_quakes=recent_quakes)
+
+
+@app.route("/location", methods=['POST'])
+def user_location():
+	user_latlong = request.form.get(point)
+
+
 
 @app.route("/quakes")
 def quake_notes():
 	quakes = Quake.query.order_by(Quake.quake_datetime.desc()).all()
 	return render_template("quakes.html", quakes=quakes)
+
 
 @app.route("/quakes/<quake_id>")
 def quake_note(quake_id):
@@ -24,8 +34,14 @@ def quake_note(quake_id):
 	quake = Quake.query.get(quake_id)
 	return render_template("quake.html", quake=quake)
 
+@app.route("/note_example")
+def note():
+	# get most recent quake record
+	quake = db_session.query(Quake).order_by(Quake.quake_datetime.desc()).first()
+	# pass dictionary k,v to jinja template
+	return render_template("quake.html", quake=quake)
 
-
+app.secret_key = "testing"
 
 if __name__=="__main__":
 	app.run(debug = True)
---
+++ static/geolocate.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    getLocation()
+});
+
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+    console.log("I am location")
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    // var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    // +latlon+"&zoom=14&size=1000x1200&sensor=false";
+    // document.getElementById("all-map").innerHTML = "<img src='"+img_url+"'>";
+    console.log(latlon)
+}
+
+$.ajax({
+  type: "POST",
+  url: "/location",
+  point: { latlon: "latlon" }
+});
+
+
+
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
