45368b9ea8589c7564b41afc39299845a79dd260
Heather Bryant <heather@rebelcompass.com>
2014-12-01 12:45:20 -0800

    Moved mapstyles to js file. Modified html templates accordingly.

---
+++ static/mapstyles.js
@@ -0,0 +1,173 @@
+var MAPSTYLES = [
+        { 
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [ 
+                { 
+                    "color": "#04162b"
+                }
+            ] 
+        },
+        { 
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [ 
+        { 
+                "visibility": "simplified"
+        }, 
+        { 
+                "color": "#cccccc"
+        }
+            ]
+        },
+        { 
+            "featureType": "water",
+            "elementType": "labels.icon",
+            "stylers": [
+                { 
+                "visibility": "simplified"
+                },
+                { 
+                "color": "#FFFFFF"
+                }
+            ]
+        },
+        { 
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "color": "#808080"
+                }
+            ]
+        },
+        {
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        { 
+            "featureType": "landscape.natural.landcover",
+            "elementType": "geometry.fill",
+            "stylers": [
+                { 
+                    "color": "#000b17"
+                }
+            ]
+        },
+        { 
+            "featureType": "landscape.natural.terrain",
+            "elementType": "geometry.fill",
+            "stylers": [ 
+                {
+                    "color": "#000b17"
+                }
+            ]
+        },
+        {   
+            "featureType": "administrative",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "color": "#808080"
+                },
+                {   
+                    "visibility": "off"
+                }
+            ]
+        },
+        { 
+            "featureType": "landscape.man_made",
+            "stylers": [ 
+                {
+                    "color": "#000b17"
+                }
+            ]
+        },
+        {   
+            "featureType": "poi.business",
+            "stylers": [
+                { 
+                    "color": "#000b17"
+                }
+            ]
+        },
+        { 
+            "featureType": "poi.attraction",
+            "stylers": [ 
+                { 
+                    "color": "#000b17"
+                }
+            ]
+        },
+        { 
+            "featureType": "landscape",
+            "stylers": [
+                {
+                    "color": "#000b17"
+                }
+            ]
+        },
+        { 
+            "featureType": "poi",
+            "stylers": [ 
+                {
+                    "color": "#000b17"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.province",
+            "stylers": [ 
+                {
+                    "visibility": "on"
+                },
+                {   
+                    "color": "#282828" 
+                }
+            ]
+        },
+        { 
+            "featureType": "administrative.province",
+            "elementType": "labels.text.fill",
+            "stylers": [ 
+                {
+                    "visibility": "off"
+                }, 
+                {
+                    "color": "#ffffff"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "stylers": [ 
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        { 
+            "featureType": "administrative.country",
+            "elementType": "geometry.stroke",
+            "stylers": [ 
+                {
+                    "color": "#0a4e6f"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.country",
+            "stylers": [ 
+                {
+                    "color": "#0a4e6f"
+                }
+            ]
+        },
+        { 
+            "featureType": "administrative.country",
+            "elementType": "geometry.fill"
+        }
+            ]
